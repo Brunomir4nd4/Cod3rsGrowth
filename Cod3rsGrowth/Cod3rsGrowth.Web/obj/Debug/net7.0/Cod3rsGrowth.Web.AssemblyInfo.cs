55e693cf674e5264938d9a38032aecb92e66b6c4
Brunomir4nd4 <bmb400289@gmail.com>
2024-05-14 13:36:55 -0300

    Não sei o que aconteceu

--- Cod3rsGrowth/Cod3rsGrowth.Web/obj/Debug/net7.0/Cod3rsGrowth.Web.AssemblyInfo.cs
+++ Cod3rsGrowth/Cod3rsGrowth.Web/obj/Debug/net7.0/Cod3rsGrowth.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cod3rsGrowth.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c5a629d72a0124a3d513fc58c50b51d4d3f490")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cod3rsGrowth.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cod3rsGrowth.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
