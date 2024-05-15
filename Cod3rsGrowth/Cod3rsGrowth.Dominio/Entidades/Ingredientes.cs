77a1ff5790f327e9a18ea0f8da3c698991c22bde
bmb40 <bmb400289@gmail.com>
2024-05-14 23:57:17 -0300

    Adicionando classes de dados

---
+++ Cod3rsGrowth/Cod3rsGrowth.Dominio/Entidades/Ingredientes.cs
@@ -0,0 +1,12 @@
+﻿using Cod3rsGrowth.Dominio.Enums;
+
+namespace Cod3rsGrowth.Dominio.Entidades
+{
+    public class Ingredientes
+    {
+        public static int Id { get; set; }
+        public string Nome { get; set; }
+        public int Qtd { get; set; }
+        public Naturalidade Naturalidade;
+    }
+}
---
+++ Cod3rsGrowth/Cod3rsGrowth.Dominio/Enums/Naturalidade.cs
@@ -0,0 +1,9 @@
+﻿namespace Cod3rsGrowth.Dominio.Enums
+{
+    public enum Naturalidade : int
+    {
+        OverWorld = 1,
+        Nether = 2,
+        TheEnd = 3
+    }
+}
