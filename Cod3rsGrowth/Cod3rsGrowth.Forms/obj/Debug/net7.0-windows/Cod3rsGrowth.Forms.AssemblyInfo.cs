d81f006b9682f87699e03103a1cb3616da6ba4fa
bmb40 <bmb400289@gmail.com>
2024-05-14 21:05:24 -0300

    Excluindo arquivo console

--- Cod3rsGrowth/Cod3rsGrowth.Forms/obj/Debug/net7.0-windows/Cod3rsGrowth.Forms.AssemblyInfo.cs
+++ Cod3rsGrowth/Cod3rsGrowth.Forms/obj/Debug/net7.0-windows/Cod3rsGrowth.Forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cod3rsGrowth.Forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f69354a0dadfdcd842d20a3c4398c848a9e3da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b5f8dbd9f16723dbee3e5a1ee94ad986b1d126")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cod3rsGrowth.Forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cod3rsGrowth.Forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
