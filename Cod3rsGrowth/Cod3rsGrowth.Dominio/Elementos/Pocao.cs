540bf8623646a01de53fcdc9b2f0e51b2251e1c4
bmb40 <bmb400289@gmail.com>
2024-05-14 23:05:21 -0300

    Adicionando a criacao das classes de dados

---
+++ Cod3rsGrowth/Cod3rsGrowth.Dominio/Elementos/Pocao.cs
@@ -0,0 +1,14 @@
+﻿namespace Cod3rsGrowth.Dominio.Elementos
+{
+    internal class Pocao
+    {
+        public static int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataDeVencimento { get; set; }
+        public bool Vencido { get; set; }
+        public decimal Valor { get; set; }
+        public int Qtd { get; set; }
+        public string Imagem { get; set; }
+    }
+}
