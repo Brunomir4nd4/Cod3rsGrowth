f874a839a2439242d56d10427e54226c208d3227
Brunomir4nd4 <bmb400289@gmail.com>
2024-05-16 13:04:24 -0300

    Adicionando interface com implementacao no repositorui mock de teste

---
+++ Cod3rsGrowth.Dominio/Interfaces/IRepositorioMock.cs
@@ -0,0 +1,9 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+
+namespace Cod3rsGrowth.Dominio.Interfaces
+{
+    public interface IRepositorioMock
+    {
+        List<Pocao> ObterTodos();
+    }
+}
---
+++ Cod3rsGrowth.Teste/RepositorioMock.cs
@@ -0,0 +1,20 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.Interfaces;
+
+namespace Cod3rsGrowth.Teste
+{
+    public class RepositorioMock : IRepositorioMock
+    {
+        public readonly List<Pocao> pocoes;
+
+        public RepositorioMock()
+        {
+
+        }
+
+        List<Pocao> IRepositorioMock.ObterTodos()
+        {
+            return pocoes;
+        }
+    }
+}
