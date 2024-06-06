ec30487f485f963e62d07280f5abac41fffdf2e9
Brunomir4nd4 <bmb400289@gmail.com>
2024-06-06 12:07:10 -0300

    Criando conexão com o SQL Server e criando os metodos crud com a interface

---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioIngrediente.cs
@@ -0,0 +1,30 @@
+﻿using Cod3rsGrowth.Dominio.Interface;
+using Cod3rsGrowth.Dominio.Entidades;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    internal class RepositorioIngrediente : IRepositorio<Ingrediente>
+    {
+        public List<Ingrediente> ObterTodos()
+        {
+            return ObterTodos();
+        }
+
+        public Ingrediente ObterPorId(int idProcurado)
+        {
+            return ObterPorId(idProcurado);
+        }
+
+        public void Criar(Ingrediente ingrediente)
+        {
+        }
+        public Ingrediente Editar(Ingrediente ingredienteEditado)
+        {
+            return ingredienteEditado;
+        }
+        public void Remover(int idIngrediente)
+        {
+
+        }
+    }
+}
---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioPocao.cs
@@ -0,0 +1,26 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.Interface;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    public class RepositorioPocao
+    {
+        public List<Pocao> ObterTodos()
+        {
+            return ObterTodos();
+        }
+
+        public Pocao ObterPorId(int idProcurado)
+        {
+            return ObterPorId(idProcurado);
+        }
+
+        public void Criar(Receita novaReceita)
+        {
+        }
+
+        public void Remover(int idPocao)
+        {
+        }
+    }
+}
