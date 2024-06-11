a9d8a9df7b29d43bfb363baa65ab35c384b9774c
Brunomir4nd4 <bmb400289@gmail.com>
2024-06-11 13:59:48 -0300

    Retirando comentários

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -7,55 +7,33 @@ namespace Cod3rsGrowth.Forms
 {
     public static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
 
             using (var serviceProvider = CreateServices())
             using (var scope = serviceProvider.CreateScope())
             {
-                // Put the database update into a scope to ensure
-                // that all resources will be disposed.
                 UpdateDatabase(scope.ServiceProvider);
             }
         }
-            /// <summary>
-            /// Configure the dependency injection services
-            /// </summary>
         private static ServiceProvider CreateServices()
         {
             return new ServiceCollection()
-                // Add common FluentMigrator services
                 .AddFluentMigratorCore()
                 .ConfigureRunner(rb => rb
-                    // Add SQLite support to FluentMigrator
                     .AddSqlServer()
-                    // Set the connection string
                     .WithGlobalConnectionString("Data Source=DESKTOP-G389DPC\\SQLEXPRESS;Initial Catalog=Cod3rsGrowthdb;Persist Security Info=True;User ID=sa;Password=sap@123;Trust Server Certificate=True")
-                    // Define the assembly containing the migrations
                     .ScanIn(typeof(Migrador).Assembly).For.Migrations())
-                // Enable logging to console in the FluentMigrator way
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
-                // Build the service provider
                 .BuildServiceProvider(false);
         }
-
-        /// <summary>
-        /// Update the database
-        /// </summary>
         private static void UpdateDatabase(IServiceProvider serviceProvider)
         {
-            // Instantiate the runner
             var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
 
-            // Execute the migrations
             runner.MigrateUp();
         }
     }
