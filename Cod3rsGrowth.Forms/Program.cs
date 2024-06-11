c2e880c99c07a65b9de5ed30ad01c443df6c3aa2
Brunomir4nd4 <bmb400289@gmail.com>
2024-06-11 09:51:18 -0300

    add classes e metodos patterns FluentMigrator

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -1,6 +1,15 @@
+using System;
+using System.Linq;
+
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+using Cod3rsGrowth.Infra;
+
 namespace Cod3rsGrowth.Forms
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -12,6 +21,46 @@ namespace Cod3rsGrowth.Forms
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                // Put the database update into a scope to ensure
+                // that all resources will be disposed.
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+            /// <summary>
+            /// Configure the dependency injection services
+            /// </summary>
+        private static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=DESKTOP-G389DPC\\SQLEXPRESS;Initial Catalog=Cod3rsGrowthdb;Persist Security Info=True;User ID=sa;Password=sap@123;Trust Server Certificate=True")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(LogTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
         }
     }
 }
---
+++ Cod3rsGrowth.Infra/LogTable.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator;
+
+namespace Cod3rsGrowth.Infra
+{
+    [Migration(20240611094700)]
+    public class LogTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Ingrediente")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString().NotNullable()
+                .WithColumn("Quantidade").AsInt32().NotNullable()
+                .WithColumn("Naturalidade").AsInt32().NotNullable();
+
+            Create.Table("Receita")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString().NotNullable()
+                .WithColumn("Descricao").AsString().NotNullable()
+                .WithColumn("Valor").AsDecimal().NotNullable()
+                .WithColumn("Imagem").AsString().NotNullable();
+
+            Create.Table("Pocao")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("IdReceita").AsInt32().NotNullable()
+                .WithColumn("Vencido").AsBoolean().NotNullable()
+                .WithColumn("DataDeFabricacao").AsDateTime().NotNullable();
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Ingrediente");
+            Delete.Table("Receita");
+            Delete.Table("Pocao");
+        }
+    }
+}
+
