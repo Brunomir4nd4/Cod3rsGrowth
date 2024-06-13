c723948982c79ee41dfd16e184b13df1aa6f5a02
Brunomir4nd4 <bmb400289@gmail.com>
2024-06-13 09:48:23 -0300

    add injeção no forms

---
+++ Cod3rsGrowth.Forms/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Cod3rsGrowth.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
